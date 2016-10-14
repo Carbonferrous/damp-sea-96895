dc572f9f37812d0108ab2e2052e561bd4e5d193b
carbonferrous <carbonferrous@gmail.com>
2016-10-13 22:51:43 -0500

    change 1

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 var HTTPS = require('https');
-var cool = require('cool-ascii-faces');
+//var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+      botRegex = /^\/Botman$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "Hello World! I'm ALIVE!!!";//cool();
 
   options = {
     hostname: 'api.groupme.com',
