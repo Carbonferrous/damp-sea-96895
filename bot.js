88f0e575e95cac232782e45c788e8e8ada1a6b3c
carbonferrous <carbonferrous@gmail.com>
2016-10-13 23:09:18 -0500

    yes

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function respond() {
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(request.text);
+    postMessage(request.text.replace(botRegex, ""));
     this.res.end();
   } else {
     console.log("don't care");
